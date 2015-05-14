da7d540722e27b05dd49b7044ebf701c2fa9ebe4
nqdinh <nqdinhddt@gmail.com>
2015-05-14 15:41:30 +0700

    query dns after having an IP address

--- examples/dns/user/user_main.c
+++ examples/dns/user/user_main.c
@@ -1,19 +1,18 @@
 #include "ets_sys.h"
 #include "os_type.h"
 #include "osapi.h"
-#include "gpio.h"
-#include "os_type.h"
 #include "ip_addr.h"
 #include "espconn.h"
 #include "user_config.h"
 #include "user_interface.h"
 #include "driver/uart.h"
 
-#define DOMAIN    "ubisen.com"
+#include "wifi.h"
+
+#define DOMAIN    "dl.dropboxusercontent.com"
 
 LOCAL struct espconn user_conn;
 LOCAL ip_addr_t esp_server_ip;
-LOCAL os_timer_t client_timer;
 
 LOCAL void ICACHE_FLASH_ATTR
 user_esp_platform_dns_found(const char *name, ip_addr_t *ipaddr, void *arg)
@@ -30,38 +29,20 @@ user_esp_platform_dns_found(const char *name, ip_addr_t *ipaddr, void *arg)
             *((uint8 *)&ipaddr->addr + 2), *((uint8 *)&ipaddr->addr + 3));
 }
 
-// LOCAL void ICACHE_FLASH_ATTR                                                    
-// user_esp_platform_dns_check_cb(void *arg)                                         
-// {                                                                               
-//     struct espconn *pespconn = arg;                                             
-//     os_printf("user_esp_platform_dns_check_cb\n");                                
-//     espconn_gethostbyname(pespconn, DOMAIN, &esp_server_ip, user_esp_platform_dns_found);
-//     os_timer_arm(&client_timer, 1000, 0);                                       
-// }
+LOCAL void ICACHE_FLASH_ATTR
+wifiConnectCb(uint8_t status)
+{
+  if(status == STATION_GOT_IP){
+    espconn_gethostbyname(&user_conn, DOMAIN, &esp_server_ip, user_esp_platform_dns_found);
+  }
+}
 
 //Init function 
 void ICACHE_FLASH_ATTR
 user_init()
 {
-    char ssid[32] = SSID;
-    char password[64] = SSID_PASSWORD;
-    struct station_config stationConf;
-
     // UART setup
     uart_init(BIT_RATE_115200, BIT_RATE_115200);
 
-    //Set station mode
-    wifi_set_opmode( 0x1 );
-
-    //Set ap settings
-    os_memcpy(&stationConf.ssid, ssid, 32);
-    os_memcpy(&stationConf.password, password, 64);
-    wifi_station_set_config(&stationConf);
-
-    esp_server_ip.addr = 0;                                                     
-    espconn_gethostbyname(&user_conn, DOMAIN, &esp_server_ip, user_esp_platform_dns_found);
-
-    // os_timer_disarm(&client_timer);
-    // os_timer_setfn(&client_timer, (os_timer_func_t *)user_esp_platform_dns_check_cb, pespconn);
-    // os_timer_arm(&client_timer, 1000, 0);
+    WIFI_Connect(SSID, SSID_PASSWORD, wifiConnectCb);
 }
---
+++ examples/dns/user/wifi.c
@@ -0,0 +1,95 @@
+/*
+ * wifi.c
+ *
+ *  Created on: Dec 30, 2014
+ *      Author: Minh
+ */
+#include "wifi.h"
+#include "user_interface.h"
+#include "osapi.h"
+#include "espconn.h"
+#include "os_type.h"
+#include "mem.h"
+
+static ETSTimer WiFiLinker;
+WifiCallback wifiCb = NULL;
+static uint8_t wifiStatus = STATION_IDLE, lastWifiStatus = STATION_IDLE;
+static void ICACHE_FLASH_ATTR wifi_check_ip(void *arg)
+{
+	struct ip_info ipConfig;
+
+	os_timer_disarm(&WiFiLinker);
+	wifi_get_ip_info(STATION_IF, &ipConfig);
+	wifiStatus = wifi_station_get_connect_status();
+	if (wifiStatus == STATION_GOT_IP && ipConfig.ip.addr != 0)
+	{
+
+		os_timer_setfn(&WiFiLinker, (os_timer_func_t *)wifi_check_ip, NULL);
+		os_timer_arm(&WiFiLinker, 2000, 0);
+
+
+	}
+	else
+	{
+		if(wifi_station_get_connect_status() == STATION_WRONG_PASSWORD)
+		{
+
+			os_printf("STATION_WRONG_PASSWORD\r\n");
+			wifi_station_connect();
+
+
+		}
+		else if(wifi_station_get_connect_status() == STATION_NO_AP_FOUND)
+		{
+
+			os_printf("STATION_NO_AP_FOUND\r\n");
+			wifi_station_connect();
+
+
+		}
+		else if(wifi_station_get_connect_status() == STATION_CONNECT_FAIL)
+		{
+
+			os_printf("STATION_CONNECT_FAIL\r\n");
+			wifi_station_connect();
+
+		}
+		else
+		{
+			os_printf("STATION_IDLE\r\n");
+		}
+
+		os_timer_setfn(&WiFiLinker, (os_timer_func_t *)wifi_check_ip, NULL);
+		os_timer_arm(&WiFiLinker, 500, 0);
+	}
+	if(wifiStatus != lastWifiStatus){
+		lastWifiStatus = wifiStatus;
+		if(wifiCb)
+			wifiCb(wifiStatus);
+	}
+}
+
+void ICACHE_FLASH_ATTR WIFI_Connect(uint8_t* ssid, uint8_t* pass, WifiCallback cb)
+{
+	struct station_config stationConf;
+
+	os_printf("WIFI_INIT\r\n");
+	wifi_set_opmode(STATION_MODE);
+	wifi_station_set_auto_connect(FALSE);
+	wifiCb = cb;
+
+	os_memset(&stationConf, 0, sizeof(struct station_config));
+
+	os_sprintf(stationConf.ssid, "%s", ssid);
+	os_sprintf(stationConf.password, "%s", pass);
+
+	wifi_station_set_config(&stationConf);
+
+	os_timer_disarm(&WiFiLinker);
+	os_timer_setfn(&WiFiLinker, (os_timer_func_t *)wifi_check_ip, NULL);
+	os_timer_arm(&WiFiLinker, 1000, 0);
+
+	wifi_station_set_auto_connect(TRUE);
+	wifi_station_connect();
+}
+
